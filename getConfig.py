9c7af6808d015318381b6fa92e41ae1c2c122d0b
Ethan Hampton <ethohampton@gmail.com>
2020-03-28 22:31:32 -0700

    Add getConfig program
    This is designed to simplify configuration
    and secret mangement

---
+++ getConfig.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+
+# load configuration settings
+# return value of setting or non-zero exit code if doesn't exit
+# settings follow test.test.test to signify "test":{"test":{"test":"This is the output"}}
+# settings live in the "config" object of root json object
+configVals = dict()
+
+
+def load_configs():
+    home = os.path.expanduser("~") + "/emh.json"
+    if not os.path.isfile(home):
+        return
+    try:
+        with open(home) as f:
+            config = json.load(f)["config"]
+            for setting in config:  # loop through all settings in config
+                configVals[setting] = config[setting]
+    except json.decoder.JSONDecodeError:
+        print("There was an error decoding the JSON file")
+
+
+load_configs()
+valToGet = sys.argv[1].split('.')
+currentConfig = configVals
+for x in valToGet:
+    try:
+        currentConfig = currentConfig[x]
+    except KeyError:
+        print("Setting %s not found" % x)
+        sys.exit(1)
+
+print(currentConfig)
